05ebe889f4b140e08c2b91b39ea924ce02334968
wangzz-yt <2294721275@email.com>
2020-04-10 15:14:56 +0800

    2020年4月10日

--- Wzz/src/com/bean/UIRuleUtil.java
+++ Wzz/src/com/bean/UIRuleUtil.java
@@ -1,5 +1,35 @@
 package com.bean;
 
-public class UIRuleUtil {
+import java.io.File;
 
+public class UIRuleUtil {
+//	public static void main(String[] args) {
+//
+//		File jpegFile = new File("F:/Í¼Æ¬¶¨Î»/DCIM/P20130116_125539.jpg");
+//		Metadata metadata;
+//		try {
+//			metadata = JpegMetadataReader.readMetadata(jpegFile);
+//			Directory exif = metadata.getDirectory(ExifDirectory.class);
+//			Iterator tags1 = exif.getTagIterator();
+//			while (tags1.hasNext()) {
+//				Tag tag = (Tag)tags1.next();
+//				System.out.println(tag);
+//			}
+//			Directory jpeg = metadata.getDirectory(JpegDirectory.class);
+//			Iterator tags2 = jpeg.getTagIterator();
+//			while (tags2.hasNext()) {
+//				Tag tag = (Tag)tags2.next();
+//				System.out.println(tag);
+//			}
+//			Directory gps = metadata.getDirectory(GpsDirectory.class);
+//			Iterator tags3 = gps.getTagIterator();
+//			while (tags3.hasNext()) {
+//				Tag tag = (Tag)tags3.next();
+//				System.out.println(tag);
+//			}
+//		} catch (JpegProcessingException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//}
 }
